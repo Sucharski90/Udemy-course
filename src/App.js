f968b70426eb14fef29fbaa6cddbf24ceb3c4744
Matt <MVSucharski@gmail.com>
2018-06-18 18:31:07 -0400

    following udemy course

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react'; 
+import Person from './Person/Person';
 import './App.css';
 
 class App extends Component {
+state = {
+  persons: [
+    { name: 'matt', age: 27},
+    { name: 'pat', age: 32},
+    { name: 'sam', age: 26}
+  ]
+}
+switchNameHandler = (newName) => {
+  // console.log('was clicked');
+  this.setState({
+    persons: [
+      { name: newName, age: 27},
+      { name: 'pat', age: 55},
+      { name: 'sam', age: 39}
+    ]
+  })
+}
   render() {
     return (
       <div className="App">
         <h1>hi</h1>
+        <button onClick={this.switchNameHandler.bind(this, "max")}>Click</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNameHandler}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
   }
